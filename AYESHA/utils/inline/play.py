cec0153a99cee7159d8f83abcaf48c73af478f02
ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd <sonkarp463@gmail.com>
2025-01-04 01:08:35 +0530

    Update play.py

--- AYESHA/utils/inline/play.py
+++ AYESHA/utils/inline/play.py
@@ -1,8 +1,7 @@
 import math
 
 from pyrogram.types import InlineKeyboardButton
-from AYESHA import app
-import config
+
 from AYESHA.utils.formatters import time_to_seconds
 
 
@@ -34,51 +33,54 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "▰▱▱▱▱▱▱▱▱▱"
+        bar = "─🅢─────────"
     elif 10 < umm < 20:
-        bar = "▰▰▱▱▱▱▱▱▱▱"
+        bar = "──🅞────────"
     elif 20 <= umm < 30:
-        bar = "▰▰▰▱▱▱▱▱▱▱"
+        bar = "───🅝───────"
     elif 30 <= umm < 40:
-        bar = "▰▰▰▰▱▱▱▱▱▱"
+        bar = "────🅐──────"
     elif 40 <= umm < 50:
-        bar = "▰▰▰▰▰▱▱▱▱▱"
+        bar = "─────♡─────"
     elif 50 <= umm < 60:
-        bar = "▰▰▰▰▰▰▱▱▱▱"
+        bar = "──────🅡────"
     elif 60 <= umm < 70:
-        bar = "▰▰▰▰▰▰▰▱▱▱"
+        bar = "───────🅐───"
     elif 70 <= umm < 80:
-        bar = "▰▰▰▰▰▰▰▰▱▱"
+        bar = "────────🅙──"
     elif 80 <= umm < 95:
-        bar = "▰▰▰▰▰▰▰▰▰▱" 
+        bar = "─────────🅐─"   
     else:
-        bar = "▰▰▰▰▰▰▰▰▰▰"
+        bar = "──────────♡"
+        
     buttons = [
-                [
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
-        [
-         InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
+      [
+            InlineKeyboardButton(
+                text="σ𝗐ɳєʀ", url="https://t.me/RISHANTTTT",
+            ),
+            InlineKeyboardButton(
+                text="sυᴘᴘσʀᴛ", url="https://t.me/+68Wnlps-CqxkYjM1",
+            )
         ],
-        [
-            InlineKeyboardButton(text="✭ ᴜᴘᴅᴀᴛᴇ ❥", url="https://t.me/RIYA_NETWORK",),
-            InlineKeyboardButton(text="✭ sᴜᴘᴘᴏʀᴛ ❥", url="https://t.me/riya_CHAT_support",),
-        ],
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
- def stream_markup(_, chat_id):
+
+def stream_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
